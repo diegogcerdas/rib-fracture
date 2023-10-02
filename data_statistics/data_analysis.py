1b109529f38ed087e0679893b5b166228f9a2cb0
eggonz <>
2023-10-02 20:45:51 +0200

    update data analytics notebook

--- data_statistics/data_analysis.py
+++ data_statistics/data_analysis.py
@@ -1,19 +1,36 @@
+"""
+Util functions module for data analysis.
+
+Contains:
+    - compute_rib_data
+    > analysis: fractures
+        - fracture_label_analysis
+    > visualization
+        - imshow
+        - imshow_mutiple
+        - scatter3d
+        - create_gif
+    > array value operations
+        - minmax
+        - histogram_equalization
+        - equalize_per_slice
+    > analysis: values
+        - analysis_cum_scans
+"""
+
 import os
+from collections import defaultdict, Counter
 
 import nibabel as nib
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from collections import defaultdict
-import itertools
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+sns.set_theme()
+import imageio
 
-DATA_ROOT = os.environ['HOME'] + '/data'
-DATASET_ROOT = DATA_ROOT + '/ribfrac'
-TRAIN_ROOT = DATASET_ROOT + '/train'
-VAL_ROOT = DATASET_ROOT + '/val'
-ANALYSIS_ROOT = DATA_ROOT + '/analysis'
 
 LABEL_CODE = {
     0: 'background',
@@ -24,57 +41,33 @@ LABEL_CODE = {
     -1: 'undefined'
 }
 
-rib_frac_types = {name: 0 for name in LABEL_CODE.values()}
-rib_data = defaultdict(list)
-nr_frac = defaultdict(int)
 
-info_files = [
-    os.path.join(TRAIN_ROOT, 'ribfrac-train-info-1.csv'),
-    os.path.join(TRAIN_ROOT, 'ribfrac-train-info-2.csv'),
-    #os.path.join(VAL_ROOT, 'ribfrac-val-info.csv')
-]
-for info_file in info_files:
-    for index, row in pd.read_csv(info_file).iterrows():
-        public_id, label_id, label_code = row
-        rib_data[public_id].append(label_code)
-        nr_frac[public_id] += 1
-        if label_code != 0:  # ignore background
-            rib_frac_types[LABEL_CODE[label_code]] += 1
-        
-print('rib_data=', rib_data)
-print('nr_frac=', nr_frac)  # nr_frac is just the len of each rib_data
-#print(rib_frac_types)
+def compute_rib_data(train_folder_path):
+    rib_data = defaultdict(list)
 
-total_rib_frac = sum(rib_frac_types.values())
-avg_frac = total_rib_frac/len(nr_frac)
-print('average fractures', avg_frac)
-print('total fractures',total_rib_frac)
+    info_files = [
+        os.path.join(train_folder_path, 'ribfrac-train-info-1.csv'),
+        os.path.join(train_folder_path, 'ribfrac-train-info-2.csv'),
+        #os.path.join(val_folder_path, 'ribfrac-val-info.csv')
+    ]
+    for info_file in info_files:
+        for index, row in pd.read_csv(info_file).iterrows():
+            public_id, label_id, label_code = row
+            rib_data[public_id].append(label_code)
 
-# def get_axis(scan, label, idx):
-#     min_x = -999
-#     max_x = -999
-#     min_y = -999
-#     max_y = -999
-# #     xs = []
-# #     ys = []
-#     for i, slice_ in enumerate(scan[idx[0]:idx[-1]+1]):
-#         #frac = np.where(slice_ == label, 1, 0)
-#         #xs.append(max(np.sum(frac, axis = 1)))
-#         #ys.append(max(np.sum(frac, axis = 0)))
-#         x, y = np.where(slice_ == label)
-#         min_x, max_x, min_y, max_y = np.min(x), np.max(x), np.min(y), np.max(y)
-#     if min_x == -999:
-#         print(len(scan), idx, scan)
-#     return min_x, max_x, min_y, max_y
+    return rib_data
 
-def get_bbox(scan):
+
+# slice geometric operations
+
+def _get_bbox(scan):
     """get the 3d bounding box of the volume. scan is binary boolean array with coords (z, x, y)"""
     z, x, y = np.where(scan)
     assert len(z) > 0, 'scan is empty'
     min_x, max_x, min_y, max_y, min_z, max_z = np.min(x), np.max(x), np.min(y), np.max(y), np.min(z), np.max(z)
     return min_x, max_x, min_y, max_y, min_z, max_z
 
-def get_max_xy_box(scan):
+def _get_max_xy_box(scan):
     """gets the largest window size that could contain the area of the facture in each slice. scan is binary boolean array with coords (z, x, y)"""
     xs = np.any(scan, axis=2, keepdims=True)  # any in y direction
     ys = np.any(scan, axis=1, keepdims=True)  # any in x direction
@@ -84,16 +77,16 @@ def get_max_xy_box(scan):
     size_y = np.max(size_y)  # largest y size
     return size_x, size_y
 
-def get_volume(scan):
+def _get_volume(scan):
     """volume of the volume defined by the binary boolean array scan with coords (z, x, y)"""
     return np.sum(scan)
 
-def get_xy_areas(scan):
+def _get_xy_areas(scan):
     """gets minimum and maximum 2d areas through z-planes. scan is binary boolean array with coords (z, x, y)"""
     areas = np.sum(scan, axis=(1, 2))  # sum in x and y direction
     return np.min(areas), np.max(areas)
 
-def center_of_mass(scan) -> tuple:
+def _center_of_mass(scan) -> tuple:
     """cneter of mass of the volume defined by the binary boolean array scan with coords (z, x, y)"""
     z, x, y = np.where(scan)
     assert len(z) > 0, 'scan is empty'
@@ -102,6 +95,8 @@ def center_of_mass(scan) -> tuple:
     com_y = np.mean(y)
     return com_z, com_x, com_y
 
+
+# analysis: fractures
 
 def fracture_label_analysis(train_folder_path, rib_data):
     """
@@ -186,6 +181,9 @@ def fracture_label_analysis(train_folder_path, rib_data):
             
         for frac_idx, frac_code in enumerate(rib_data[public_id]):
 
+            if frac_code == 0:  # ignore background
+                continue
+
             # idx= list of slice indices where the fracture is present
             # idx = []
             # for ind in df_slices.index:
@@ -205,10 +203,10 @@ def fracture_label_analysis(train_folder_path, rib_data):
             ## compute values
 
             reduced_scan = label_scan[idxs[0]:idxs[-1]+1]  # ease computation with only relevant slices
-            min_x, max_x, min_y, max_y, min_z, max_z = get_bbox(reduced_scan == frac_idx)
-            volume = get_volume(reduced_scan == frac_idx)
-            max2dsize_x, max2dsize_y = get_max_xy_box(reduced_scan == frac_idx)
-            min2darea, max2darea = get_xy_areas(reduced_scan == frac_idx)
+            min_x, max_x, min_y, max_y, min_z, max_z = _get_bbox(reduced_scan == frac_idx)
+            volume = _get_volume(reduced_scan == frac_idx)
+            max2dsize_x, max2dsize_y = _get_max_xy_box(reduced_scan == frac_idx)
+            min2darea, max2darea = _get_xy_areas(reduced_scan == frac_idx)
 
             # compensate for reduced scan offset
             min_z += idxs[0]
@@ -218,7 +216,7 @@ def fracture_label_analysis(train_folder_path, rib_data):
 
             # again reduce scan to only relevant volume
             reduced_scan = label_scan[idxs[0]:idxs[-1]+1, min_x:max_x+1, min_y:max_y+1]
-            com_z, com_x, com_y = center_of_mass(reduced_scan == frac_idx)  # com relative to chunk
+            com_z, com_x, com_y = _center_of_mass(reduced_scan == frac_idx)  # com relative to chunk
 
             # compensate for reduced scan offset
             com_x += min_x
@@ -309,16 +307,160 @@ def fracture_label_analysis(train_folder_path, rib_data):
             scan_size = scan_sizes.loc[df_frac['public_id'], 'size_' + axis].reset_index(drop=True)
             df_frac[rel_attr] = df_frac[abs_attr] / scan_size
 
+    print('Done!')
+
     return df_scan, df_frac
 
-exists = os.path.exists(os.path.join(ANALYSIS_ROOT, 'scan.csv'))
-if exists:
-    df_scan = pd.read_csv(os.path.join(ANALYSIS_ROOT, 'scan.csv'))
-    df_frac = pd.read_csv(os.path.join(ANALYSIS_ROOT, 'frac.csv'))
-else:
-    df_scan, df_frac = fracture_label_analysis(TRAIN_ROOT, rib_data)
-    os.makedirs(ANALYSIS_ROOT, exist_ok=True)
-    df_scan.to_csv(os.path.join(ANALYSIS_ROOT, 'scan.csv'), index=False)
-    df_frac.to_csv(os.path.join(ANALYSIS_ROOT, 'frac.csv'), index=False)
 
-print(df_frac.describe())
+# visualization
+
+def imshow(img, title=None, range=None):
+    sns.reset_orig()
+    vmin, vmax = range if range is not None else (img.min(), img.max())
+    plt.imshow(img, cmap='gray', vmin=vmin, vmax=vmax)
+    if title:
+        plt.title(title)
+    plt.show()
+    sns.set_theme()
+
+def imshow_multiple(imgs: list, titles: list=None, suptitle: list=None, range=None):
+    sns.reset_orig()
+    n_images = len(imgs)
+    fig, axes = plt.subplots(1, len(imgs), figsize=(5*n_images, 5))
+    for i, im in enumerate(imgs):
+        vmin, vmax = range if range is not None else (im.min(), im.max())
+        axes[i].imshow(im, cmap='gray', vmin=vmin, vmax=vmax)
+        if titles:
+            axes[i].set_title(titles[i])
+    if suptitle:
+        fig.suptitle(suptitle)
+    plt.tight_layout()
+    plt.show()
+    sns.set_theme()
+
+
+def scatter3d(x, y, z, hue, title=None, view_angle=(30, 20)):
+
+    # axes instance
+    fig = plt.figure(figsize=(6,6))
+    ax = Axes3D(fig, auto_add_to_figure=False)
+    fig.add_axes(ax)
+
+    # default seaborn colormap
+    colors = {
+        'buckle':'tab:blue',
+        'displaced':'tab:orange',
+        'non-displaced':'tab:green',
+        'undefined':'tab:red',
+        'segmental':'tab:purple',
+    }
+    colors = [colors[h] for h in hue]
+
+    # plot
+    sc = ax.scatter(x, y, z, s=20, label=hue, color=colors, marker='o', alpha=.2)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # set view angle
+    ax.view_init(*view_angle)
+
+    # legend
+    plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=2)
+
+    if title:
+        plt.title(title)
+    plt.show()
+
+
+def create_gif(scan, save_path):
+    """create animated gif through z-axis from array with shape (z, x, y). array values must be in [0, 1]"""
+    images = []
+    for i in range(scan.shape[0]):
+        slice = scan[i]
+        slice = np.uint8(slice * 255)
+        images.append(slice)
+    imageio.mimsave(save_path, images, duration=0.1)
+    print('saved gif at', save_path)
+
+
+# array value operations
+
+def minmax(image, range=None):
+    """normalize image using minmax normalization"""
+    if range is not None:
+        min_, max_ = range
+    else:
+        min_ = np.min(image)
+        max_ = np.max(image)
+    return (image - min_) / (max_ - min_)
+
+def histogram_equalization(image):
+    """
+    equalize image using histogram equalization.
+    input image is a float array with values in [0, 1].
+    output is in range [0, 1].
+    src: https://saturncloud.io/blog/mastering-histogram-equalization-in-python-without-numpy-or-plotting/
+    """
+    # convert to uint8
+    image = (image * 255).astype(np.uint8)
+    # calculate histogram
+    histogram = Counter(image.flatten())
+    # calculate cdf
+    cdf = dict()
+    cum_sum = 0
+    for intensity, freq in sorted(histogram.items()):
+        cum_sum += freq
+        cdf[intensity] = cum_sum
+    # normalize cdf
+    cdf_min = min(cdf.values())
+    normalized_cdf = {k: ((v-cdf_min)/(image.size-1))*255 for k, v in cdf.items()}
+    # equalize image
+    if len(image.shape) == 2:
+        equalized_image = [[normalized_cdf[pixel] for pixel in row] for row in image]
+    elif len(image.shape) == 3:
+        equalized_image = [[[normalized_cdf[pixel] for pixel in row] for row in slice_] for slice_ in image]
+    # convert back to float
+    equalized_image = np.asarray(equalized_image) / 255.
+    return equalized_image
+
+def equalize_per_slice(scan):
+    """equalizes each slice independently"""
+    equalized_scan = np.zeros(scan.shape)
+    for i in range(scan.shape[0]):
+        equalized_scan[i] = histogram_equalization(scan[i])
+    return equalized_scan
+
+
+# analysis: pixel values
+
+def analysis_pixel_values(train_folder_path, fn_preprocess):
+    """
+    Accumulates all scans in the train folder after preprocessing them with fn_preprocess
+
+    Args:
+        train_folder_path: path to the train folder
+        fn_preprocess: function that takes a scan and returns the scan preprocessed. It must return a binary boolean array with coords (z, x, y)
+    """
+    max_num_slices = 721  # hardcoded from df_scan.describe()>size_z>max
+
+    cum_scans = np.zeros((max_num_slices, 512, 512))
+
+    for filename in tqdm(os.listdir(train_folder_path)[2:], desc='overlaping slices'):
+
+        if not (filename.endswith("image.nii.gz") or filename.endswith("image.nii")):
+            continue
+
+        # read scan
+        filepath = os.path.join(train_folder_path, filename)
+        raw_scan = nib.load(filepath).get_fdata().T.astype(float)
+
+        # threshold scan
+        scan = fn_preprocess(raw_scan)  # (z, x, y)
+
+        # accumulate slices
+        cum_scans[:scan.shape[0]] += scan
+
+    print('Done!')
+
+    return cum_scans
