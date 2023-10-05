2ec1eadf6751f721975081711540682e6d160b08
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2023-10-05 13:39:36 +0200

    Integrate unet3plus into regular files.

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from torch import nn, optim
+from torch import optim
 from tqdm import tqdm
+from unet3plus.model_unet3plus import Unet3Plus
 
 
 class UnetModule(pl.LightningModule):
@@ -25,7 +26,7 @@ class UnetModule(pl.LightningModule):
         self.weight_decay = weight_decay
         self.data_root = data_root
 
-        self.network = Unet(n_channels)
+        self.network = Unet3Plus(n_channels)
 
     def forward(self, x):
         return self.network(x)
@@ -139,74 +140,3 @@ class UnetModule(pl.LightningModule):
 
     def on_validation_epoch_end(self) -> None:
         self.compute_eval("val")
-
-
-class Unet(nn.Module):
-    def __init__(self, n_channels):
-        super(Unet, self).__init__()
-        self.n_channels = n_channels
-
-        def double_conv(in_channels, out_channels):
-            return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-            )
-
-        def down(in_channels, out_channels):
-            return nn.Sequential(
-                nn.MaxPool2d(2), double_conv(in_channels, out_channels)
-            )
-
-        class up(nn.Module):
-            def __init__(self, in_channels, out_channels, bilinear=True):
-                super().__init__()
-
-                if bilinear:
-                    self.up = nn.Upsample(
-                        scale_factor=2, mode="bilinear", align_corners=True
-                    )
-                else:
-                    self.up = nn.ConvTranpose2d(
-                        in_channels // 2, in_channels // 2, kernel_size=2, stride=2
-                    )
-
-                self.conv = double_conv(in_channels, out_channels)
-
-            def forward(self, x1, x2):
-                x1 = self.up(x1)
-                # [?, C, H, W]
-                diffY = x2.size()[2] - x1.size()[2]
-                diffX = x2.size()[3] - x1.size()[3]
-
-                x1 = F.pad(
-                    x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2]
-                )
-                x = torch.cat([x2, x1], dim=1)  ## why 1?
-                return self.conv(x)
-
-        self.inc = double_conv(self.n_channels, 64)
-        self.down1 = down(64, 128)
-        self.down2 = down(128, 256)
-        self.down3 = down(256, 512)
-        self.down4 = down(512, 512)
-        self.up1 = up(1024, 256)
-        self.up2 = up(512, 128)
-        self.up3 = up(256, 64)
-        self.up4 = up(128, 64)
-        self.out = nn.Conv2d(64, 1, kernel_size=1)
-
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        return self.out(x)
